3f26cf8c29cdeb56a830ab5f700320c40034b54f
ionelDumitru <ionel.dumitru04@e-uvt.ro>
2026-03-11 00:36:08 +0200

    add workspace variable

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import argparse
 import pytest
+import os 
 
+workspace = os.environ.get('WORKSPACE')
 def api_security_tests():
-    pytest.main(["API", "-v", "--cache-clear"])
+    pytest.main(["{workspace}/scm/tests/API/", "-v", "--cache-clear"])
 
 def main():
     parser = argparse.ArgumentParser(description='Parser for verification tests runner')
