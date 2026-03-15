ae700067202c1689b397a3cb866258f92861ebd3
ionelDumitru <ionel.dumitru04@e-uvt.ro>
2026-03-16 00:56:04 +0200

    force color in bandit

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -13,7 +13,7 @@ def api_security_tests():
 
 def automated_security_checks():
     subprocess.run(
-        ["bandit", f"{workspace}/CRM-backend/app/app.py", ""],
+        ["bandit", f"{workspace}/CRM-backend/app/app.py", "--format", "text", "-ll", "--color"],
         check=True
     )
 
