d102961c462fe93267c572fb1bfc3fbfe6c8d30e
ionelDumitru <ionel.dumitru04@e-uvt.ro>
2026-03-16 00:52:49 +0200

    fix path x2

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -13,7 +13,7 @@ def api_security_tests():
 
 def automated_security_checks():
     subprocess.run(
-        ["bandit", f"{workspace}/CRM-backend/API/app.py", ""],
+        ["bandit", f"{workspace}/CRM-backend/app/app.py", ""],
         check=True
     )
 
