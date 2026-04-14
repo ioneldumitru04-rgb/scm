668875059cc4079d4cbe50bfd5f7c3dd6b29c67f
ionelDumitru <ionel.dumitru04@e-uvt.ro>
2026-04-14 15:27:47 +0300

    Edit ss

--- common/common.sh
+++ common/common.sh
@@ -28,6 +28,8 @@ function success() {
 
 function git_push_snapshot() {
     local branch="$1"
+    git config --global user.email "ionel.dumitru04@e-uvt.ro"
+    git config --global user.name "ionelDumitru"
     git add .
     git commit -m "SS: Added new snapshot"
     git push origin ${branch}
